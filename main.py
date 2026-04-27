27365970925c664a6e976e7b9b35c226b8602a0f
Spoorthi3011 <121597323+Spoorthi3011@users.noreply.github.com>
2026-04-27 16:47:02 +0100

    Implement basic grid search rescue environment simulation

---
+++ main.py
@@ -0,0 +1,18 @@
+from env.grid_env import GridSearchRescueEnv
+import random
+import time
+
+env = GridSearchRescueEnv(grid_size=8, n_agents=2, n_victims=3, n_obstacles=5)
+
+obs = env.reset()
+
+done = False
+
+while not done:
+    actions = [random.randint(0, 4) for _ in range(env.n_agents)]
+
+    obs, rewards, done, _ = env.step(actions)
+
+    env.render()
+    print("Rewards:", rewards)
+    time.sleep(0.3)
